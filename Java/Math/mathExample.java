9bfb5e3df9628273f496ccfbe069ee320baead1e
Randeep710 <35597182+Randeep710@users.noreply.github.com>
2021-08-16 15:19:12 +0530

    Update mathExample.java
    Solution to PowerOfThree problem.

--- Java/Math/mathExample.java
+++ Java/Math/mathExample.java
@@ -1,1 +1,42 @@
+/*
+Problem Statement:
 
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+
+Input: n = 27
+Output: true
+Example 2:
+
+Input: n = 0
+Output: false
+Example 3:
+
+Input: n = 9
+Output: true
+Example 4:
+
+Input: n = 45
+Output: false
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+ 
+
+Follow up: Could you solve it without loops/recursion?
+*/
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+      
+        if(n==0)
+            return false;
+        
+        return (int) (Math.ceil(Math.log(n)/Math.log(3))) == (int) (Math.floor(Math.log(n)/Math.log(3)));
+    }
+}
